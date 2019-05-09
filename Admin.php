8786fd9daad5490d41d234cc3da0d7b0e220aff2
Alexandre Tremblay <alexandre.tremblay255@gmail.com>
2019-05-09 16:58:53 -0400

    Update Admin.php
    AJout de code pour les boutons de la page Admin

--- Admin.php
+++ Admin.php
@@ -9,7 +9,8 @@
 			echo("<td>$user[0]</td>");
 			echo("<td>$user[1]</td>");
 			echo("<td>$user[2]</td>");
-			echo("<td><img>")
+			echo("<td><button type='button' class = 'btn'><span class='glyphicon glyphicon-refresh'/></button></td>");
+            echo("<td><button type='button' class = 'btn'><span class='glyphicon glyphicon-remove'/></button></td>");
 			echo("</tr>");
 		}
 		
@@ -19,6 +20,10 @@
 <html>
 	<head>
 		<title>Section administrateur - Galerie image</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	</head>
 	
 	<body>
