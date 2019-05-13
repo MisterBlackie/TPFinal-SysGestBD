e014346216d5b2a3c43d02511bfffeb2dc3de2fe
MisterBlackie <alexrevenger234@hotmail.com>
2019-05-13 15:03:24 -0400

    Header
    Commencement du layout de header

---
+++ Layout/Header.php
@@ -0,0 +1,13 @@
+<header>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Galerie Photo</a>
+			</div>
+			
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="">Images</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
