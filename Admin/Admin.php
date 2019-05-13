2850cfd56fb3814eb929046cc6d1f1b30a61d7c6
MisterBlackie <alexrevenger234@hotmail.com>
2019-05-13 14:33:18 -0400

    Layout
    Ajout de layout:
    
    - Header et footer: page contenant les en-tête et pied de page du projet
    
    - Ajout de style.css: style général pour toutes les pages
    
    POUR UTILISER LES LAYOUTS:
    - Inclure le style.css dans la page (html)
    - Inclure avec PHP les layout aux endroits voulues

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -25,7 +25,7 @@
 		<title>Section administrateur - Galerie image</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-		<link rel = "stylesheet" href = "style.css">
+		<link rel = "stylesheet" href = "../style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	</head>
@@ -42,6 +42,8 @@
 					<?php listUsers(); ?>
 				</table>
 		</main>
+		
+		<?php include("../layout/footer.php"); ?>
 	</body>
 </html>
 
