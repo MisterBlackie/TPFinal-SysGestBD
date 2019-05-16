a8c4fcc9246723449804c29a6ac1566374b52c14
bibitomate <47693060+bibitomate@users.noreply.github.com>
2019-05-16 16:40:16 -0400

    Add files via upload

--- Inscription/index.php
+++ Inscription/index.php
@@ -1,6 +1,5 @@
 <?php 
   session_start(); 
-
   if (!isset($_SESSION['pseudo'])) {
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
@@ -100,7 +99,95 @@ mysqli_close($connection);
     <?php  if (isset($_SESSION['pseudo'])) : ?>
     	<p>Bienvenue <strong><?php echo $_SESSION['pseudo']; ?></strong></p>
     	<p> <a href="index.php?logout='1'" style="color: red;">Se Déconnecter</a> </p>
+		
+		
     <?php endif ?>
+	
+	<?php 
+	$bdd = new PDO('mysql:host=167.114.152.54;dbname=dbequipe35;charset=utf8', 'equipe35', '264mqnh9');
+	$stmt1 = $bdd->prepare("select * from Image" );
+	$total = $stmt1->execute();
+	echo "<div class='grid-container'>";
+	while ($donnees = $stmt1->fetch())
+		{
+			$test = $donnees[3]; 
+			$image = $test;
+			$imageData = base64_encode(file_get_contents($image));
+			echo "<div class='grid-item'>";
+			echo '<img src="data:image/jpeg;base64,'.$imageData.' "style="width:200px;height:150px; padding:0px 10px 0px 10px;">';
+			echo "<br>";
+			echo "titre :".$donnees[1] ."<br> description". $donnees[2] . "<br> usager". $donnees[4]  . "<br> Date". $donnees[5];
+			
+			echo "</div>";
+}
+	echo "</div>";
+$stmt1->closeCursor();
+?>
+	
+	<form action="index.php" method="post"
+        enctype="multipart/form-data" >
+    <input type="hidden" name="MAX_FILE_SIZE" value="5000000">
+    Fichier : <input name="fichier" size="35" type="file">	
+	<div class="input-group">
+  		<label>titre</label>
+  		<input type="text" name="titre" >
+  	</div>
+  	<div class="input-group">
+  		<label>description</label>
+  		<input type="textarea" name="description">
+  	</div>
+	<div class="input-group">
+	<button type="submit" class="btn" name="ajouter">Ajouter une photo</button>
+    </div>
+	</form>
+	
+	
+	
+	<?php
+	if (isset($_POST['ajouter']))
+	{
+			unset($_POST['ajouter']);
+	$rep = 'Image/';
+	$fich = $rep . basename($_FILES['fichier']['name']);
+
+	if (move_uploaded_file($_FILES['fichier']['tmp_name'], $fich)) {
+}
+else
+{
+	echo"error";
+}
+
+
+$stmt1 = $bdd->prepare("call InsertImage(?,?,?,?)" , ARRAY(PDO::ATTR_CURSOR , PDO::CURSOR_FWDONLY));
+	
+
+	$stmt1->bindParam(1,$titre);
+	$stmt1->bindParam(2,$description);
+	$stmt1->bindParam(3,$url);
+	$stmt1->bindParam(4,$Membre_pseudo);
+	
+	
+	$titre = $_POST['titre'];
+  $description =  $_POST['description'];
+  $url =  $fich;
+  $Membre_pseudo =  $_SESSION['pseudo'];
+	
+
+	
+	//Executer requette
+	
+
+	$total = $stmt1->execute();
+
+	}
+	
+	
+	
+	?>
+	
+
+	
+</form>
 </div>
 		
 </body>
